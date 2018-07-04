c19e7283f617814d8ccea8a504b03fd0e0332167
FEI Shenghao <feishenghao@gmail.com>
2018-07-04 12:03:52 +0800

    计算到知识点分值

--- shujia_data_process.sql
+++ shujia_data_process.sql
@@ -45,86 +45,81 @@ FROM tb_exam_question AS t1 JOIN view_examid AS v1
 ON  t1.examid = v1.examid;
 
 
---本次知识点表1.0 本次所遇到的知识点及对应的试题
-DROP TABLE IF EXISTS dw_tb_point_v1;
-CREATE external TABLE IF NOT EXISTS dw_tb_point_v1(
-	pointid STRING COMMENT '知识点ID',
-	problemid STRING COMMENT '试题ID'
+--本次试题知识点关系表 本次所遇到的试题及对应的知识点
+DROP TABLE IF EXISTS dw_tb_question_point_v1;
+CREATE external TABLE IF NOT EXISTS dw_tb_question_point_v1(
+	problemid STRING COMMENT '试题ID',
+	pointid STRING COMMENT '知识点ID'
 );
-INSERT INTO TABLE dw_tb_point_v1
-SELECT pointid, problemid
+INSERT INTO TABLE dw_tb_question_point_v1
+SELECT problemid,pointid
 FROM tb_exam_question_point AS t1 JOIN dw_tb_problem_v1 AS t2
 ON  t1.examquestionid = t2.problemid;
 
-
---本次试题表2.0 添加试题对应知识点
-DROP TABLE IF EXISTS dw_tb_problem_v2;
-CREATE external TABLE IF NOT EXISTS dw_tb_problem_v2(
-	problemid STRING COMMENT '本次试题ID',
-	score FLOAT COMMENT '本试题分值',
-	pointid STRING COMMENT '试题对应知识点ID'
-);
-INSERT INTO TABLE dw_tb_problem_v2
-SELECT t1.problemid,score,pointid
-FROM dw_tb_problem_v1 AS t1 JOIN dw_tb_point_v1 AS t2
-ON t1.problemid = t2.problemid;
-
-
---本次试题表3.0 添加试题包含知识点占比，一题中有几个知识点
-DROP TABLE IF EXISTS dw_tb_problem_v3;
-CREATE external TABLE IF NOT EXISTS dw_tb_problem_v3(
-	problemid STRING COMMENT '本次试题ID',
-	score FLOAT COMMENT '本试题分值',
-	pointid STRING COMMENT '试题对应知识点ID',
+--本次试题知识点关系表2.0 添加试题包含知识点占比，一题中有几个知识点
+DROP TABLE IF EXISTS dw_tb_question_point_v2;
+CREATE external TABLE IF NOT EXISTS dw_tb_question_point_v2(
+	problemid STRING COMMENT '试题ID',
+	pointid STRING COMMENT '知识点ID',
 	proportion FLOAT COMMENT '试题对应 N个知识点，值为 1/N'
 );
-INSERT INTO TABLE dw_tb_problem_v3
-SELECT t1.problemid,score,pointid,proportion
-FROM dw_tb_problem_v2 AS t1 JOIN 
+INSERT INTO TABLE dw_tb_question_point_v2
+SELECT t1.problemid,t1.pointid,proportion
+FROM dw_tb_question_point_v1 AS t1 JOIN 
 (SELECT problemid,1/COUNT(pointid) AS proportion
-FROM dw_tb_problem_v2
+FROM dw_tb_question_point_v1
 GROUP BY problemid) t2
 ON t1.problemid = t2.problemid;
 
 
---本次知识点表2.0
-DROP TABLE IF EXISTS dw_tb_point_v2;
-CREATE external TABLE IF NOT EXISTS dw_tb_point_v2(
+--本次知识点表1.0
+DROP TABLE IF EXISTS dw_tb_point_v1;
+CREATE external TABLE IF NOT EXISTS dw_tb_point_v1(
 	pointid STRING COMMENT '知识点ID',
 	pointname STRING COMMENT '知识点名称',
-	problemid STRING COMMENT '试题ID',
 	parentid STRING COMMENT '父节点ID',
 	topid STRING COMMENT '一级知识点ID',
 	classhour FLOAT COMMENT '学时',
 	frequency INT COMMENT '出现频率'
 );
-INSERT INTO TABLE dw_tb_point_v2
-SELECT t2.pointid,pointname,t2.problemid,parentid,topid,classhour,frequency
+INSERT INTO TABLE dw_tb_point_v1
+SELECT t2.pointid,pointname,parentid,topid,classhour,frequency
 FROM tb_syllabus_points AS t1 
 JOIN view_syllabusid AS v1 ON t1.syllabusid = v1.syllabusid
-JOIN dw_tb_point_v1 AS t2 ON t1.pointid = t2.pointid;
+JOIN dw_tb_question_point AS t2 ON t1.pointid = t2.pointid;
 
 
 -- 本次知识点表3.0 计算知识点分值
-DROP TABLE IF EXISTS dw_tb_point_v3;
-CREATE external TABLE IF NOT EXISTS dw_tb_point_v3(
+DROP TABLE IF EXISTS dw_tb_point_v2;
+CREATE external TABLE IF NOT EXISTS dw_tb_point_v2(
 	pointid STRING COMMENT '知识点ID',
 	pointname STRING COMMENT '知识点名称',
-	pointscore FLOAT COMMENT '本次考试该知识点分值',
-	problemid STRING COMMENT '试题ID',
 	parentid STRING COMMENT '父节点ID',
 	topid STRING COMMENT '一级知识点ID',
 	classhour FLOAT COMMENT '学时',
 	frequency INT COMMENT '出现频率',
 	pointscore FLOAT COMMENT '知识点分值'
 );
-INSERT INTO TABLE dw_tb_point_v3
-SELECT t1.pointid,t1.pointname,pointscore,t1.problemid,parentid,topid,classhour,frequency
-FROM dw_tb_point_v2 AS t1
-JOIN (SELECT pointid,SUM(score*proportion) AS pointscore
-FROM dw_tb_problem_v3
-GROUP BY pointid) AS t2 
-ON t1.pointid = t2.pointid;
+INSERT INTO TABLE dw_tb_point_v2
+SELECT t4.pointid,t4.pointname,t4.parentid,t4.topid,t4.classhour,t4.frequency,t3.pointscore
+FROM 
+(SELECT t2.pointid, SUM(problemscore*proportion) AS pointscore
+FROM dw_tb_problem_v1 AS t1
+JOIN dw_tb_question_point_v2 AS t2
+ON t1.problemid = t2.problemid
+GROUP BY t2.pointid) AS t3
+JOIN dw_tb_point_v1 AS t4
+ON t3.pointid = t4.pointid;
+
+
+
+
+
+
+
+
+
+
 
 DROP TABLE IF EXISTS dw_tb_problem_score_v2;
 CREATE external TABLE IF NOT EXISTS dw_tb_problem_score_v2(
